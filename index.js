b8b910b36dfd8fce4196a90a317570083e1a9d4e
Zachary <zachary.t.rich@gmail.com>
2017-02-20 16:10:29 -0500

    Added connection to heroku and mongodb

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const express = require('express')
+const mongodb = require('mongodb')
+const bodyParser = require('body-parser')
+
 const app = express()
-const port = 3000 //for local testing until I deploy to Heroku - Zach
+app.use(bodyParser.json())
+
+//const port = 3000
+const db
+
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+  if (err) {
+    console.log('error:'err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");
+
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
 
 app.get('/',(request,response)=>{
-    response.send('Hello from express')
+    response.send('Welcome to upgrade')
 })
 
-app.listen(port, (err)=>{
+/*app.listen(port, (err)=>{
     if (err) return console.log('error', err)
     console.log('server is listening on ' + port)
-})
+})*/
