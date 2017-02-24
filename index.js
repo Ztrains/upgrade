327819e529b9e7ee1811cfa5fced46f6b941edfe
Curtis Maves <cmaves@purdue.edu>
2017-02-23 20:09:48 -0500

    Setup https server in index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const express = require('express')
-const mongodb = require('mongodb')
-const bodyParser = require('body-parser')
+const fs = require('fs'); // required to read https certs
+const https = require('https');
+const express = require('express');
+const mongodb = require('mongodb');
+const bodyParser = require('body-parser');
+const app = express();
 
-const app = express()
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
 
 //const port = 3000
 var db;
@@ -28,8 +31,18 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI || localTestUrl, function (e
 
 app.get('/',(request,response)=>{
     response.send('Welcome to upgrade')
-})
+});
 
+//setup https credentials
+var privKey = fs.readFileSync('ourprivKey.key', 'utf8');
+var certificate = fs.readFileSync('ourcert.crt', 'utf8');
+var options = {key: privKey, cert: certificate};
+
+//setup a https server server to listen on port 3000
+var httpsServer = https.createServer(options, app);
+httpsServer.listen(3000);
+
+//probably shouldn't setup a unsecure conenction to start
 /*app.listen(port, (err)=>{
     if (err) return console.log('error', err)
     console.log('server is listening on ' + port)
