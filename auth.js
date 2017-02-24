e419d20a024424852f25d1badbfda8bd485dfb39
Curtis Maves <curtismaves@gmail.com>
2017-02-24 16:57:27 -0500

    Added local password auth strategy to auth.py

--- auth.js
+++ auth.js
@@ -3,4 +3,17 @@ Module used will be passport, because it will be easy to expand future
 auth stragegies in the future.*/
 
 const passport = require('passport');
+const bcrypt = require('bcrypt');
 const LocalStrategy = require('passport-local').Strategy);
+
+passport.use(new LocalStrategy(
+    function(email, password, users, done) {
+        users.find( {email: email}, function (err, user) {
+          if(err) 
+            return  done(err);
+          if(!user || !bcrypt.compareSync(password, user.hash))
+            return done(null, false, {message: 'Incorrect email/password combo'});
+          done(null, user)
+        });
+    });
+});
