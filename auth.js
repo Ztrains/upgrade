0f11f8d0c26f2d5d3c23ac08695a23e84887cedd
Curtis Maves <curtismaves@gmail.com>
2017-02-24 10:15:57 -0500

    Create auth.js for auth stuff

---
+++ auth.js
@@ -0,0 +1,6 @@
+/*used for authentication of login and sessions
+Module used will be passport, because it will be easy to expand future
+auth stragegies in the future.*/
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy);
