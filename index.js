170bcab3de8469e5b857909f39161eb85659cd64
Zachary <zachary.t.rich@gmail.com>
2017-03-03 00:25:28 -0500

    hopefully fixed timeout issue

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ app.get('/join/:class', stormpath.authenticationRequired, (req,res)=>{
           res.redirect('/')
         }
         else {
-            collection.update({_id: req.params.class}, {$pull: {students: req.user.userName}})
+            collection.update({_id: ""+req.params.class}, {$pull: {students: ""+req.user.userName}})
         }
     })
 
