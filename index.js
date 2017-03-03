b6b172616a16fe26d1ac850a1bb62269de36aecb
Zachary <zachary.t.rich@gmail.com>
2017-03-03 00:34:53 -0500

    testing

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ app.get('/join/:class', stormpath.authenticationRequired, (req,res)=>{
           res.redirect('/')
         }
         else {
-            collection.update({_id: ""+req.params.class}, {$push: {students: ""+req.user.username}})
+            collection.update({_id: ObjectId(req.params.class)}, {$push: {students: ""+req.user.username}})
         }
     })
 
