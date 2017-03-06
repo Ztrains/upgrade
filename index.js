4061211163aa7bb93ef41e799b9a94d2fa1702ff
Zachary <zachary.t.rich@gmail.com>
2017-03-06 15:42:11 -0500

    removed unused file and make file look nicer

--- index.js
+++ index.js
@@ -8,63 +8,65 @@ const favicon = require('serve-favicon');
 var app = express();
 
 app.use(stormpath.init(app, {
-  expand: {
-    customData: true
-  },
-  web: {
-    login: {
-      enabled: true,
-      nextUri: "/"
+    expand: {
+        customData: true
+    },
+    web: {
+        login: {
+            enabled: true,
+            nextUri: "/"
+        }
     }
-  }
 }))
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 app.use(favicon(__dirname + '/docs/favicon.ico'))
 
 //const port = 3000
 var db;
 var localTestUrl = 'mongodb://localhost:27017/test'
 
-mongodb.MongoClient.connect(process.env.MONGODB_URI || localTestUrl, function (err, database) {
-  if (err) {
-    console.log('ERROR:', err);
-    res.redirect('/')
-  }
+mongodb.MongoClient.connect(process.env.MONGODB_URI || localTestUrl, function(err, database) {
+    if (err) {
+        console.log('ERROR:', err);
+        res.redirect('/')
+    }
 
-  // Save database object from the callback for reuse.
-  db = database;
-  console.log("Database connection ready");
+    // Save database object from the callback for reuse.
+    db = database;
+    console.log("Database connection ready");
 
-  // Initialize the app.
-  app.on('stormpath.ready', function() {
-    var server = app.listen(process.env.PORT || `8080`, function () {
-        var port = server.address().port;
-        console.log("App now running on port", port);
+    // Initialize the app.
+    app.on('stormpath.ready', function() {
+        var server = app.listen(process.env.PORT || `8080`, function() {
+            var port = server.address().port;
+            console.log("App now running on port", port);
+        });
     });
-  });
 });
 
-app.get('/',stormpath.authenticationRequired, (req,res)=>{
+app.get('/', stormpath.authenticationRequired, (req, res) => {
     res.send('Welcome to upgrade, ' + req.user.givenName)
 });
 
-app.get('/classList', /*stormpath.authenticationRequired,*/ (req,res)=> {
+app.get('/classList', /*stormpath.authenticationRequired,*/ (req, res) => {
     //res.type('json');
     var list;
-    db.collection('classes', (err, collection)=>{
+    db.collection('classes', (err, collection) => {
         if (err) {
-          console.log('ERROR:', err);
-          res.redirect('/')
-        }
-        else {
-            collection.find({},{_id:1}).toArray((err, ret)=>{
-                if (err){
+            console.log('ERROR:', err);
+            res.redirect('/')
+        } else {
+            collection.find({}, {
+                _id: 1
+            }).toArray((err, ret) => {
+                if (err) {
                     console.log('ERROR:', err)
                     res.redirect('/')
-                }
-                else {
+                } else {
                     res.json(ret)
                 }
             })
@@ -72,15 +74,23 @@ app.get('/classList', /*stormpath.authenticationRequired,*/ (req,res)=> {
     })
 })
 
-app.get('/join/:class/:type', stormpath.authenticationRequired, (req,res)=>{
+app.get('/join/:class/:type', stormpath.authenticationRequired, (req, res) => {
     var list;
-    db.collection('classes', (err, collection)=>{
+    db.collection('classes', (err, collection) => {
         if (err) {
-          console.log('ERROR:', err);
-          res.redirect('/')
-        }
-        else {
-            collection.update({_id: req.params.class}, {$push: {students: {name: req.user.fullName, type: req.params.type}}})
+            console.log('ERROR:', err);
+            res.redirect('/')
+        } else {
+            collection.update({
+                _id: req.params.class
+            }, {
+                $push: {
+                    students: {
+                        name: req.user.fullName,
+                        type: req.params.type
+                    }
+                }
+            })
         }
     })
 
@@ -90,39 +100,36 @@ app.get('/join/:class/:type', stormpath.authenticationRequired, (req,res)=>{
     res.redirect('/')
 })
 
-app.get('/name/:new', stormpath.authenticationRequired, (req,res)=>{
+app.get('/name/:new', stormpath.authenticationRequired, (req, res) => {
     req.user.givenName = req.params.new;
-    req.user.save(function (err) {
-      if (err) {
-        res.status(400).end('Oops!  There was an error: ' + err.userMessage);
-      }
-      else {
-        res.end('Name was changed!');
-      }
-    });
-})
-
-app.get('/email/:new', stormpath.authenticationRequired, (req,res)=>{
-    req.user.email = req.params.new;
-    req.user.username = req.params.new;
-    req.user.save(function (err) {
-      if (err) {
-        res.status(400).end('Oops!  There was an error: ' + err.userMessage);
-      }
-      else {
-        res.end('Email/Username was changed!');
-      }
-    });
-})
-
-app.get('/info/:type/:name', stormpath.authenticationRequired, (req,res)=>{
-    req.user.customData[req.params.type] = req.params.name;
-
-    req.user.customData.save(function(err){
+    req.user.save(function(err) {
         if (err) {
             res.status(400).end('Oops!  There was an error: ' + err.userMessage);
+        } else {
+            res.end('Name was changed!');
         }
-        else {
+    });
+})
+
+app.get('/email/:new', stormpath.authenticationRequired, (req, res) => {
+    req.user.email = req.params.new;
+    req.user.username = req.params.new;
+    req.user.save(function(err) {
+        if (err) {
+            res.status(400).end('Oops!  There was an error: ' + err.userMessage);
+        } else {
+            res.end('Email/Username was changed!');
+        }
+    });
+})
+
+app.get('/info/:type/:val', stormpath.authenticationRequired, (req, res) => {
+    req.user.customData[req.params.type] = req.params.val;
+
+    req.user.customData.save(function(err) {
+        if (err) {
+            res.status(400).end('Oops!  There was an error: ' + err.userMessage);
+        } else {
             res.end('Custom Data added!')
         }
     })
