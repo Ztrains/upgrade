93dbc92dc8a361efe4ffc82eece0d3c376ef77a1
Curtis Maves <cmaves@purdue.edu>
2017-03-27 14:40:33 -0400

    Reenabled passport in index.js

--- index.js
+++ index.js
@@ -2,32 +2,29 @@ const fs = require('fs'); // required to read https certs
 const https = require('https');
 const express = require('express');
 const mongodb = require('mongodb');
+const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const stormpath = require('express-stormpath');
 const favicon = require('serve-favicon');
+const bcrypt = require('bcryptjs');
+const auth = require('./auth.js');
 var app = express();
-
-app.use(stormpath.init(app, {
-    expand: {
-        customData: true
-    },
-    web: {
-        login: {
-            enabled: true,
-            nextUri: "/"
-        }
-    }
-}))
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
-}))
-app.use(favicon(__dirname + '/docs/favicon.ico'))
+}));
+app.use(auth.initialize());
+app.use(auth.session());
+app.use(cookieParser());
+app.use(require('express-session')({ secret: 'This is a good secret', resave: false, saveUninitialized: false }));
 
-//const port = 3000
+app.use(favicon(__dirname + '/docs/favicon.ico'));
+
+const port = 3000;
 var db;
-var localTestUrl = 'mongodb://localhost:27017/test'
+var users; //users collection;
+var localTestUrl = 'mongodb://localhost:27017/test';
 
 mongodb.MongoClient.connect(process.env.MONGODB_URI || localTestUrl, function(err, database) {
     if (err) {
@@ -37,22 +34,56 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI || localTestUrl, function(er
 
     // Save database object from the callback for reuse.
     db = database;
+	users = database.collection("users");
+	module.exports.users = users; //export users collection from module for use in auth.js
     console.log("Database connection ready");
+});
 
-    // Initialize the app.
-    app.on('stormpath.ready', function() {
-        var server = app.listen(process.env.PORT || `8080`, function() {
-            var port = server.address().port;
-            console.log("App now running on port", port);
-        });
-    });
+//function logs in
+app.post('/login', auth.authenticate('local', { failureRedirect: '/login' }),
+  function(req, res) {
+	if(!res.headersSent) {res.send('you have authenticated properly').redirect('/')};
+  });
+
+//function gets salt for user for possible use with HTTP Basic authentication
+app.post('/basic/salt', function(req, res) {
+	//return salt for requested user;	
+	users.findOne({email: req.body.email}, function(err, r) {
+		if(err) {console.log("basic/salt err!!"); res.send("Database error");}
+		else if(!r) {console.log("basic/salt user not found"); res.send("User doesn't exist");}
+		console.log("result of salt search: " + r.email);
+		res.json({email: r.email, salt: bcrypt.getSalt(r.hash)});//.send("Salt found");
+	});
+});
+
+app.post('/basic/test', auth.authenticate('basic', {session: false}), function(req, res) {
+	console.log("basic/test succeeded for " + req.body.email);
+	if(!res.headersSent) {res.send('Test auth succeeded');}
+});
+
+
+//registers a user in the user database
+app.post('/reg', function(req, res) {
+	users.findOne({email: req.body.email}, function(err, r) {
+		if(err) {res.send("Database error");}
+		else if(r) {res.send("User exists");}
+		var salt = bcrypt.genSaltSync(10);
+		var hash = bcrypt.hashSync(req.body.password, salt);
+		users.insertOne({email: req.body.email, hash: hash}, function(err, r) {
+			if(err) {res.send("Database error");}
+			else {res.send("Success adding user");}
+			
+		}); 
+
+	});
+
 });
 
 app.get('/', stormpath.authenticationRequired, (req, res) => {
-    res.send('Welcome to upgrade, ' + req.user.givenName)
+    res.send('Welcome to upgrade, ' + req.user.givenName);
 });
 
-app.get('/classList', /*stormpath.authenticationRequired,*/ (req, res) => {
+app.get('/classList',  (req, res) => {
     //res.type('json');
     var list;
     db.collection('classes', (err, collection) => {
@@ -109,7 +140,7 @@ app.get('/name/:new', stormpath.authenticationRequired, (req, res) => {
             res.end('Name was changed!');
         }
     });
-})
+});
 
 app.get('/email/:new', stormpath.authenticationRequired, (req, res) => {
     req.user.email = req.params.new;
@@ -121,8 +152,7 @@ app.get('/email/:new', stormpath.authenticationRequired, (req, res) => {
             res.end('Email/Username was changed!');
         }
     });
-})
-
+});
 app.get('/info/:type/:val', stormpath.authenticationRequired, (req, res) => {
     req.user.customData[req.params.type] = req.params.val;
 
@@ -130,7 +160,9 @@ app.get('/info/:type/:val', stormpath.authenticationRequired, (req, res) => {
         if (err) {
             res.status(400).end('Oops!  There was an error: ' + err.userMessage);
         } else {
-            res.end('Custom Data added!')
+            res.end('Custom Data added!');
         }
-    })
-})
+    });
+});
+
+app.listen(port);
