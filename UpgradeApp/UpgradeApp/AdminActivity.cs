812a9dead30a95499749981ccceee147a826cd6b
myers259 <myers259@purdue.edu>
2017-04-27 01:13:29 -0400

    adding requested and reported

--- UpgradeApp/UpgradeApp/AdminActivity.cs
+++ UpgradeApp/UpgradeApp/AdminActivity.cs
@@ -21,8 +21,18 @@ namespace UpgradeApp
         {
             reportedUsers = HTTPHandler.getReports();
             base.OnCreate(savedInstanceState);
-            SetContentView(Resource.Layout.AdminScreen);
+            SetContentView(Resource.Layout.adminScreen);
+            Button reportedUserButton = FindViewById<Button>(Resource.Id.reportButton);
+            Button requestClassButton = FindViewById<Button>(Resource.Id.requestClassButton);
 
+            reportedUserButton.Click += (Sender, e) =>
+            {
+
+            };
+            requestClassButton.Click += (Sender, e) =>
+            {
+
+            };
             ListView listView = FindViewById<ListView>(Resource.Id.reportedUsers);
 
             listView.Adapter = new ReportedAdapter(this, reportedUsers.reportedUsers);//Needs to be replaced with students from the server
---
+++ UpgradeApp/UpgradeApp/requestedClassesActivity.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace UpgradeApp
+{
+    [Activity(Label = "requestedClassesActivity")]
+    public class requestedClassesActivity : Activity
+    {
+        ClassList classes; //Replaced by requested classes from the server
+        int location = 0;
+        string[] items; //All the class names are placed into this 
+        
+
+        void ListView_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            //Need a variable for the approved classes
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.requestedClassScreen);
+            Button submit = FindViewById<Button>(Resource.Id.submitClassButtonR);
+            ListView listView = FindViewById<ListView>(Resource.Id.requestedClasses);
+            items = classes.classes;
+            ArrayAdapter lister = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItemChecked, items);
+            listView.Adapter = lister;
+            listView.ChoiceMode = ChoiceMode.Multiple;
+            listView.ItemClick += ListView_ItemClick;
+
+
+
+            submit.Click += (object sender, EventArgs e) =>
+            {
+                var intent = new Android.Content.Intent(this, typeof(AdminActivity));
+                //Updating the server with the new classes and remove the rest of the classes from the requested class list
+                StartActivity(intent);
+            };
+
+
+            // Create your application here
+        }
+    }
+}
