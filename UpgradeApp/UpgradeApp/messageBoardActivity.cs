a0cbfe98887675542f25cd4f968ce9ee08ce3c0d
myers259 <myers259@purdue.edu>
2017-04-27 15:15:56 -0400

    added refresh button to messageBoard

--- UpgradeApp/UpgradeApp/messageBoardActivity.cs
+++ UpgradeApp/UpgradeApp/messageBoardActivity.cs
@@ -18,31 +18,34 @@ namespace UpgradeApp
         ListView listView;
         List<chatClass> chats;
         string[] names;
+        GetChatID cid;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            SetTheme(Android.Resource.Style.ThemeMaterialLight);
+            //SetTheme(Android.Resource.Style.ThemeMaterialLight);
 
             SetContentView(Resource.Layout.messageBoard);
+            var toolbar = FindViewById<Toolbar>(Resource.Id.toolbarMB);
+            SetActionBar(toolbar);
             ActionBar.Title = Intent.GetStringExtra("theClass");
-            GetChatID cid = HTTPHandler.startABoard(Intent.GetStringExtra("theClass"));
+            cid = HTTPHandler.startABoard(Intent.GetStringExtra("theClass"));
             Messages ms = HTTPHandler.getMessages(cid._id);
             string nameOfStudent = Intent.GetStringExtra("nameOf");
-           
-            
+
+
             Button sendButton = FindViewById<Button>(Resource.Id.sendButton);
             TextView msgTextView = FindViewById<TextView>(Resource.Id.msg);
             TextView userName = FindViewById<TextView>(Resource.Id.userName);
             listView = FindViewById<ListView>(Resource.Id.message);
             chats = new List<chatClass>();
-            
+
             if (ms != null && ms.messages != null)
             {
                 for (int i = 0; i < ms.messages.Length; i++)
                 {
                     bool direction = true;
-                    if(nameOfStudent.Equals(HTTPHandler.getName(ms.messages[i].sender)))
+                    if (nameOfStudent.Equals(HTTPHandler.getName(ms.messages[i].sender)))
                     {
                         direction = false;
                     }
@@ -65,28 +68,53 @@ namespace UpgradeApp
 
             messageBoardAdapter adapt = new messageBoardAdapter(this, chats, names);
             listView.Adapter = adapt;
-           
 
 
-            sendButton.Click += (object Sender, EventArgs e) => {
-                HTTPHandler.sendMessageBoard(cid._id, Intent.GetStringExtra("theClass"), msgTextView.Text);
-                chatClass chatter = new chatClass(false, msgTextView.Text);
-                chats.Add(chatter);
-                msgTextView.Text = "";
-                ms = HTTPHandler.getMessages(cid._id);
-                if (ms != null)
+
+            sendButton.Click += (object Sender, EventArgs e) =>
+            {
+                if (!msgTextView.Text.Equals(""))
                 {
-                    names = new string[ms.messages.Length];
-                    for (int i = 0; i < ms.messages.Length; i++)
+                    HTTPHandler.sendMessageBoard(cid._id, Intent.GetStringExtra("theClass"), msgTextView.Text);
+                    chatClass chatter = new chatClass(false, msgTextView.Text);
+                    chats.Add(chatter);
+                    msgTextView.Text = "";
+                    ms = HTTPHandler.getMessages(cid._id);
+                    if (ms != null)
                     {
-                        names[i] = HTTPHandler.getName(ms.messages[i].sender);
+                        names = new string[ms.messages.Length];
+                        for (int i = 0; i < ms.messages.Length; i++)
+                        {
+                            names[i] = HTTPHandler.getName(ms.messages[i].sender);
+                        }
                     }
+                    adapt = new messageBoardAdapter(this, chats, names);
+                    listView.Adapter = null;
+                    listView.Adapter = adapt;
                 }
-                adapt = new messageBoardAdapter(this, chats, names);
-                listView.Adapter = null;
-                listView.Adapter = adapt;
             };
-            // Create your application here
+        }
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.messageBoardToolbar, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item) //Passed in the menu item that was selected
+        {
+            Messages ms = HTTPHandler.getMessages(cid._id);
+            if (ms != null && ms.messages != null)
+            {
+                names = new string[ms.messages.Length];
+                for (int i = 0; i < ms.messages.Length; i++)
+                {
+                    names[i] = HTTPHandler.getName(ms.messages[i].sender);
+                }
+            }
+            messageBoardAdapter adapt = new messageBoardAdapter(this, chats, names);
+            listView.Adapter = null;
+            listView.Adapter = adapt;
+            return base.OnOptionsItemSelected(item);
         }
     }
 }
